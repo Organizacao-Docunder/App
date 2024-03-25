69616d79e16a1a2b372cdd8c0361cf7eaece1f6f
Pedro Souza <pedrops244@hotmail.com>
2024-03-25 12:46:42 -0300

    fix: adicionado o interceptor no module global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,6 +5,9 @@ import { UserModule } from './user/user.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CheckJsonInterceptor } from './auth/interceptors/check-json-interceptor.interceptor';
 
 @Module({
   imports: [UserModule, AuthModule, ConfigModule.forRoot()],
@@ -15,6 +17,10 @@ import { ConfigModule } from '@nestjs/config';
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CheckJsonInterceptor,
     },
   ],
 })
