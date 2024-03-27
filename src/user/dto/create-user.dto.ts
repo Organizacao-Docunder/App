118a37e1a7e07880224f2ac6ef6c89fc47983597
Pedro Souza <pedrops244@hotmail.com>
2024-03-27 11:17:02 -0300

    fix: mudança no regex do nome, agora ele precisa de pelo menos um alfabetico

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -28,7 +28,7 @@ export class CreateUserDto extends User {
   @MaxLength(74)
   @IsString()
   @IsNotEmpty()
-  @Matches(/^[a-zA-Z\u00C0-\u017F\s]+$/, {
+  @Matches(/^(?=.*[a-zA-Z\u00C0-\u017F])[\w\s\u00C0-\u017F]+$/, {
     message: 'The name should only contain letters and spaces.',
   })
   name: string;
