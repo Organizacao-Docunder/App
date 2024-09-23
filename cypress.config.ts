79b1d275b3ad317d3e3a0eff01568fee0d557ff5
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-09-23 18:11:26 -0300

    fix(tests): loading cypress variables from environment

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'cypress';
+import 'dotenv/config';
 
 export default defineConfig({
-  projectId: 's4aruy',
   e2e: {
     baseUrl: 'http://localhost:3001',
     env: {
       hideCredentials: true,
       requestMode: true,
       apiUrl: 'http://localhost:3000',
+      USER_EMAIL: process.env.CYPRESS_USER_EMAIL,
+      USER_PASSWORD: process.env.CYPRESS_USER_PASSWORD,
     },
     experimentalRunAllSpecs: true,
     specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
