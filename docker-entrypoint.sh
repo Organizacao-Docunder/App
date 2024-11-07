220b49b825ab95bade5353fe80f4add8658ed17b
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-11-06 22:24:30 -0300

    fix: prisma schema path with dockercompose (#41)
    * fix: adjusting prisma.schema path on docker
    
    * fix: adjusting docker-entrypoint.sh path
    
    * fix: upgrade backend command on dockercompose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ echo "Waiting for Postgres to start..."
 wget -qO- https://raw.githubusercontent.com/eficode/wait-for/v2.2.4/wait-for | sh -s -- postgres:5432
 
 echo "Migrating the databse..."
-npx prisma migrate deploy
+npx prisma migrate deploy --schema=/app/dist/prisma/schema.prisma
 
 echo "Starting the server..."
 node dist/main.js
